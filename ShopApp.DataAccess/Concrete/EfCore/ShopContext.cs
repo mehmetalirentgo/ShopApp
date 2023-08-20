58e961d8bf25702db66612f008eb8b54a234e3d2
Mehmet Ali Turgut <mehmetali.turgut@rentgo.com>
2023-08-20 21:58:31 +0300

    connection string

---
+++ ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+    public class ShopContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
